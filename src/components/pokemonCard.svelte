402c81cf2061889a972f77864a88ef7545c89bc2
zack-jack <zjackson@redventures.com>
2021-11-03 21:33:24 -0400

    adds fade animation to pokemon cards

--- src/components/pokemonCard.svelte
+++ src/components/pokemonCard.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { fade } from 'svelte/transition';
+
 	export let id;
 	export let name;
 	export let imageSrc;
@@ -11,6 +13,7 @@
 	  p-6 bg-gray-100 rounded-md
 	  shadow-sm hover:shadow-md
 	"
+	transition:fade
 >
 	<img src={imageSrc} alt={name} class="w-40 h-40" />
 	<p class="text-xl text-gray-800 text-center uppercase">{id}. {name}</p>
